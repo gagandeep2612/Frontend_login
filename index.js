2b72d0988e1e3f603883cfd2b1b4b91c908937ae
gagandeep2612 <64344859+gagandeep2612@users.noreply.github.com>
2020-06-05 21:56:31 +0530

    Add files via upload

---
+++ index.js
@@ -0,0 +1,20 @@
+var x = document.getElementById("login");
+var y = document.getElementById("register");
+var z = document.getElementById("active");
+
+function f2() {
+  x.style.left = "-400px";
+  y.style.left = "25px";
+  z.style.left = "110px";
+}
+function f1() {
+  x.style.left = "25px";
+  y.style.left = "450px";
+  z.style.left = "0";
+}
+function f3() {
+  alert("Welcome back");
+}
+function f4() {
+  alert("Sucessfully Logged in");
+}
